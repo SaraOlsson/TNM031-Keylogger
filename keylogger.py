5daf41ae04c8b1fab2b095f310b5c373a2730cd7
SaraOlsson <sarol688@student.liu.se>
2019-10-07 11:45:20 +0200

    initial commit

---
+++ keylogger.py
@@ -0,0 +1,17 @@
+
+
+class KeyLogger:
+    def __init__(self):
+
+        self.example_variable = "hello"
+
+    def run(self):
+
+        print("running")
+
+def main_program():
+
+    logger = KeyLogger()
+    logger.run()
+
+main_program()
